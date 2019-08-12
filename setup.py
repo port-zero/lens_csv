b2f75d168036ecf6afb9777966a01c98f49ed3ae
hellerve <veit@veitheller.de>
2019-08-12 18:28:16 +0200

    vbump

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@
 import os
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='lens_csv',
     author='Veit Heller',
-    version='0.0.1',
+    version='0.0.3',
     license='MIT',
     url='https://github.com/port-zero/lens_csv',
-    download_url = 'https://github.com/port-zero/lens_csv/tarball/0.0.2',
+    download_url = 'https://github.com/port-zero/lens_csv/tarball/0.0.3',
     description='A CSV parser for lens',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages('.'),
     install_requires=[
         "pygments",
